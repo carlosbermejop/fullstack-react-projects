52edb518900c3e8a082ddc7c3f40bc79269f5d8b
Carlos Bermejo Pérez <carlosbermejop@gmail.com>
2021-01-29 20:13:28 +0100

    Skeleton finished

--- skeleton-app/config/config.js
+++ skeleton-app/config/config.js
@@ -6,3 +6,5 @@ const config = {
         process.env.MONGO_HOST ||
         `mongodb://${process.env.IP || 'localhost'}:${process.env.MONGO_PORT || '27017'}/mernproject`
 }
+
+export default config;
